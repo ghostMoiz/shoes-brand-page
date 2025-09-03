2b4a46fdb6ec97b9a4dec44edabd03c77700947c
Abdul Moiz <moizkhalid330@gmail.com>
2025-09-03 15:40:02 +0500

    Latest Code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,9 @@
+import '../App.css'
 const Navigation = () => {
     return (
         <div><nav>
             <div className="logo">
-                <img src="images/brand_logo.png" alt="brand logo" />
+                <img src="/images/brand_logo.png" alt="brand logo" />
             </div>
             <ul>
                 <li href="#">Menu</li>
